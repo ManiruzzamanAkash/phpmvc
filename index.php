ddbaf712457daf366363f5a692075c8a897eb430
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-07-19 08:21:54 +0600

    📦 NEW: Setup for homepage and composer

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+// Autoload the vendor
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load from environment variables.
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('ROOT', __DIR__);
+define('VIEWS', __DIR__ . '/views');
+define('BASE_DIR', isset($_ENV['BASE_DIR']) ? $_ENV['BASE_DIR'] : '');
+// define()
+
+
+/* Load external routes file */
+require_once 'routes/route.php';
+
+SimpleRouter::setDefaultNamespace('\App\Controllers');
+// Start the routing
+SimpleRouter::start();
