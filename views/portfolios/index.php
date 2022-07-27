8c9b2477d16e53790e5a6051384979c96f01fb0d
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-07-27 07:36:21 +0600

    🐛 FIX: Remove app title from portfolios page

--- views/portfolios/index.php
+++ views/portfolios/index.php
@@ -1,6 +1,6 @@
 <?php views('/partials/header.php'); ?>
 
-<h2>Portfolios - <?php echo env('APP_TITLE'); ?></h2>
+<h2>Portfolios</h2>
 <?php foreach($portfolios as $portfolio): ?>
     <li><?php echo $portfolio['title']; ?></li>
 <?php endforeach;?>
